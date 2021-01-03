179d0a965b1f0a95173cd250ad093194ab450a87
Rob Sweny <Robsweny@gmail.com>
2021-01-03 21:58:50 +0000

    Allow user to open and close compose message

--- Contactjavascript.js
+++ Contactjavascript.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $("#minimize").mouseenter(function() {
       $(this).children('img').attr("src", "images/gmail_icons/minimize_higlighted.png");
     });
+
     $("#minimize").mouseout(function() {
         $(this).children('img').attr("src", "images/gmail_icons/minimize.png");
     });
@@ -9,14 +10,26 @@ $(document).ready(function() {
     $("#exit_full").mouseenter(function() {
         $(this).children('img').attr("src", "images/gmail_icons/exit_full_screen_highlighted.png");
     });
+    
     $("#exit_full").mouseout(function() {
         $(this).children('img').attr("src", "images/gmail_icons/exit_full_screen.png");
     });
 
     $("#close").mouseenter(function() {
-    $(this).children('img').attr("src", "images/gmail_icons/close_highlighted.png");
+        $(this).children('img').attr("src", "images/gmail_icons/close_highlighted.png");
     });
+
     $("#close").mouseout(function() {
         $(this).children('img').attr("src", "images/gmail_icons/close.png");
     });
 });
+
+function composeMessage(){
+    var compose = document.getElementById('google_email');
+    if (document.getElementById('google_email').style.display === "none"){
+        compose.style.display = "block";
+    } else {
+        compose.style.display = "none";
+    }
+    
+}
