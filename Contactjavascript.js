0028b32b19e76002ed7c9b1aa88b57ca5ad1caac
Rob Sweny <Robsweny@gmail.com>
2021-01-06 17:58:27 +0000

    Working bottom nav bar

--- Contactjavascript.js
+++ Contactjavascript.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
     $("#minimize").mouseenter(function() {
       $(this).children('img').attr("src", "images/gmail_icons/minimize_higlighted.png");
-    });
-
-    $("#minimize").mouseout(function() {
+    }).mouseout(function() {
         $(this).children('img').attr("src", "images/gmail_icons/minimize.png");
     });
 
     $("#exit_full").mouseenter(function() {
         $(this).children('img').attr("src", "images/gmail_icons/exit_full_screen_highlighted.png");
-    });
-    
-    $("#exit_full").mouseout(function() {
+    }).mouseout(function() {
         $(this).children('img').attr("src", "images/gmail_icons/exit_full_screen.png");
     });
 
     $("#close").mouseenter(function() {
         $(this).children('img').attr("src", "images/gmail_icons/close_highlighted.png");
+    }).mouseout(function() {
+        $(this).children('img').attr("src", "images/gmail_icons/close.png");
     });
 
-    $("#close").mouseout(function() {
-        $(this).children('img').attr("src", "images/gmail_icons/close.png");
+    $("#nav_div").mouseenter(function() {
+        $(this).css("background-color","#f2f2f2");
+    }).mouseout(function() {
+        $(this).css("background-color","");
     });
 });
 
